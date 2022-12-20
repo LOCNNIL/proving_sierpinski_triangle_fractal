48b64d001ca01ec8413fcdc2bc7a778fb9a12530
Lincoln Wallace <lincolnble@hotmail.com>
2022-12-20 00:06:53 -0300

    feat: testing bevy game engine

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use bevy::prelude::*;
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Elaina Proctor".to_string())));
+    commands.spawn((Person, Name("Renzo Hume".to_string())));
+    commands.spawn((Person, Name("Zayna Nieves".to_string())));
+}
+
+fn hello_world() {
+    println!("hello world!");
+}
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_startup_system(add_people)
+        .add_system(hello_world)
+        .run();
 }
