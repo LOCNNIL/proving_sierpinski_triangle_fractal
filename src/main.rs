688584da0e5d5f160b647c75bafdbf528905a317
Lincoln Wallace <lincolnble@hotmail.com>
2022-12-24 21:26:38 -0300

    feat: Interation with components
    Making a system that "greets" the people.

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,12 @@ fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("Zayna Nieves".to_string())));
 }
 
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in query.iter() {
+        println!("hello {}!", name.0);
+    }
+}
+
 fn hello_world() {
     println!("hello world!");
 }
