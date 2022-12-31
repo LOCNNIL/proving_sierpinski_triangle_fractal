bedb1e2cc8188abed094ecca041219a3ec458a0b
Lincoln Wallace <lincolnble@hotmail.com>
2022-12-31 19:12:28 -0300

    refactor: Cleanup, removing example code of Bevy engine

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,28 @@
 use bevy::prelude::*;
-use bevy_input::prelude::*;
+//use bevy_input::prelude::*;
 
-#[derive(Component)]
-struct Person;
-
-#[derive(Component)]
-struct Name(String);
-
-pub struct HelloPlugin;
-
-impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut App) {
-        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
-            .add_startup_system(add_people)
-            .add_system(greet_people);
-    }
-}
-
-fn add_people(mut commands: Commands) {
-    commands.spawn((Person, Name("Elaina Proctor".to_string())));
-    commands.spawn((Person, Name("Renzo Hume".to_string())));
-    commands.spawn((Person, Name("Zayna Nieves".to_string())));
-}
-
-#[derive(Resource)]
-struct GreetTimer(Timer);
-
-fn greet_people(
-    time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
-    // update our timer with the time elapsed since the last update
-    // if that caused the timer to finish, we say hello to everyone
-    if timer.0.tick(time.delta()).just_finished() {
-        for name in query.iter() {
-            println!("hello {}!", name.0);
-        }
-    }
-}
-
-
-fn setup(mut commands: Commands) {
-    // Cretes a red point at position (0,0)
-    commands.spawn(SpriteComponents {
-        material: Material::colored(Color::rgb(1.0, 0.0, 0.0)),
-        transform: Transform::from_translation(Vec3::new(0.0, 0.0, 0.0)),
-        ..Default::default()
-    });
-}
 
 // Quick note: systems run in parallel by default whenever possible
 fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_plugin(HelloPlugin)
+    App::build()
+        .add_default_plugins()
         .add_startup_system(setup.system())
         .run();
+}
+
+fn setup(mut commands: Commands) {
+    // crie um ponto vermelho na posição (0, 0)
+    commands.spawn(SpriteComponents {
+        material: Material {
+            albedo: Color::rgb(1.0, 0.0, 0.0).into(),
+            ..Default::default()
+        },
+        transform: Transform::from_translation(Vec3::new(0.0, 0.0, 0.0)),
+        sprite: Sprite {
+            size: Vec2::new(10.0, 10.0),
+            ..Default::default()
+        },
+        ..Default::default()
+    });
 }
 
