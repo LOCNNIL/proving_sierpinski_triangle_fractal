21a33df7b0970862e9dfef763e4606375abae7cf
Lincoln Wallace <lincolnble@hotmail.com>
2023-01-01 20:41:13 -0300

    feat: Testing example '2D / sprite_flipping'

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,23 @@
 use bevy::prelude::*;
-//use bevy_input::prelude::*;
 
-
-// Quick note: systems run in parallel by default whenever possible
 fn main() {
-    App::build()
-        .add_default_plugins()
-        .add_startup_system(setup.system())
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
         .run();
 }
 
-fn setup(mut commands: Commands) {
-    // crie um ponto vermelho na posição (0, 0)
-    commands.spawn(SpriteComponents {
-        material: Material {
-            albedo: Color::rgb(1.0, 0.0, 0.0).into(),
-            ..Default::default()
-        },
-        transform: Transform::from_translation(Vec3::new(0.0, 0.0, 0.0)),
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn(SpriteBundle {
+        texture: asset_server.load("branding/icon.png"),
         sprite: Sprite {
-            size: Vec2::new(10.0, 10.0),
-            ..Default::default()
+            // Flip the logo to the left
+            flip_x: false,
+            // And don't flip it upside-down ( the default )
+            flip_y: true,
+            ..default()
         },
-        ..Default::default()
+        ..default()
     });
 }
-
